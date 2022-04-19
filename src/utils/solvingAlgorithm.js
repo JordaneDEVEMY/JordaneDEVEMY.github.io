61887efd0b6e98e42731e0020093c2d8332972f9
Jordane DEVÉMY <Jordane.devemy@gmail.com>
2022-04-19 12:47:12 +0200

    SolvingAlgorithm ajustment

--- src/utils/solvingAlgorithm.js
+++ src/utils/solvingAlgorithm.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /**
  *
  * ██████╗ ██╗   ██╗██████╗ ██╗███╗   ██╗██████╗ ███████╗██████╗
@@ -120,7 +121,7 @@ function isValidSudoku(gameArr) {
   return true;
 }
 
-function solveSudoku(gameArr) {
+export default function solveSudoku(gameArr) {
   const emptySpot = nextEmptySpot(gameArr);
   const r = emptySpot[0];
   const c = emptySpot[1];
@@ -148,20 +149,20 @@ function solveSudoku(gameArr) {
 
 // ---------------------------------------Test---------------------------------------
 
-const gameArr = [
-  [2, 0, 3, 0, 0, 8, 6, 0, 7],
-  [1, 4, 0, 7, 2, 6, 0, 0, 9],
-  [5, 0, 7, 1, 3, 9, 4, 2, 8],
-  [0, 2, 5, 0, 8, 1, 9, 0, 4],
-  [4, 1, 0, 9, 0, 3, 2, 0, 5],
-  [0, 7, 9, 2, 0, 5, 0, 3, 6],
-  [6, 0, 2, 0, 1, 0, 0, 9, 3],
-  [7, 0, 0, 5, 0, 2, 0, 0, 1],
-  [0, 8, 1, 3, 6, 7, 0, 4, 0],
-];
+// const gameTestArr = [
+//   [2, 0, 3, 0, 0, 8, 6, 0, 7],
+//   [1, 4, 0, 7, 2, 6, 0, 0, 9],
+//   [5, 0, 7, 1, 3, 9, 4, 2, 8],
+//   [0, 2, 5, 0, 8, 1, 9, 0, 4],
+//   [4, 1, 0, 9, 0, 3, 2, 0, 5],
+//   [0, 7, 9, 2, 0, 5, 0, 3, 6],
+//   [6, 0, 2, 0, 1, 0, 0, 9, 3],
+//   [7, 0, 0, 5, 0, 2, 0, 0, 1],
+//   [0, 8, 1, 3, 6, 7, 0, 4, 0],
+// ];
 
-solveSudoku(gameArr);
+// solveSudoku(gameTestArr);
 
-console.log('Solved');
-console.log(gameArr);
-console.log(isValidSudoku(gameArr));
+// console.log('Solved');
+// console.log(gameTestArr);
+// console.log(isValidSudoku(gameTestArr));
